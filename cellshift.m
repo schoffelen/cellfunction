34ae92bc16019204391a45bbedabcc8841061407
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2017-08-14 10:07:09 +0200

    ENH - allow for channel specific shifts

--- cellshift.m
+++ cellshift.m
@@ -2,12 +2,36 @@ function [x] = cellshift(x, shift, dim, maxshift)
 
 % CELLSHIFT(X, SHIFT, DIM) 
 
-if numel(shift)>1
+siz = size(shift);
+if siz(1)==1 && siz(2)==1
+  % this is OK, continue below
+elseif siz(1)==1 && siz(2)>1
+  % call the function recursively, and concatenate, same set of shifts for
+  % all channels
   x = x(:)';     
   y = cell(numel(shift),numel(x));
   for k = 1:numel(shift)
     %y(k,:) = cellshift(x, shift(k), dim, max(abs(shift)));
     y(k,:) = cellshift(x, shift(k), dim, [abs(min(shift)) abs(max(shift))]);
+  end
+  for k = 1:size(y,2)
+    y{1,k} = cat(1,y{:,k});
+  end
+  x = y(1,:);
+  return;
+elseif siz(1)>1 && siz(2)>=1
+  % different shifts, call the function recursively, and concatenate.
+  % note that the order of the rows in the output is different from when
+  % the same shift applies to all channels
+  edges =[abs(nanmin(shift(:))) abs(nanmax(shift(:)))];
+  y     = cell(siz(1),numel(x));
+  for k = 1:siz(1)
+    tmpshift = shift(k,:);
+    tmpshift(~isfinite(tmpshift)) = [];
+    if isempty(tmpshift)
+      tmpshift = 0;
+    end
+    y(k,:) = cellshift(cellrowselect(x, k), tmpshift, dim, edges);
   end
   for k = 1:size(y,2)
     y{1,k} = cat(1,y{:,k});
@@ -23,7 +47,6 @@ else
     error('the value for maxshift should be >= shift');
   end
 end
-
 
 if nargin<3
   dim = find(size(x{1})>1, 1, 'first');
