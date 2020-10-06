b576ad1172ca81765ccd0e964aa5adf6501981b5
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-10-06 16:25:18 +0200

    ENH - added function

--- @cell/imag.m
+++ @cell/imag.m
@@ -1,6 +1,6 @@
 function output = imag(input)
 
-% output the real part of the data in the input cells
+% output the imaginary part of the data in the input cells
 output = cell(size(input));
 for k = 1:numel(input)
   output{k} = imag(input{k});
